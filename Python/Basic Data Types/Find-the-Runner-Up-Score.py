370c5a92ee827920992f22f732a569f32e691ae6
sajibAdhi <60180521+Shiftgithub@users.noreply.github.com>
2021-04-30 08:41:38 +0600

    Find the Runner-Up Score!

--- Python/Basic Data Types/Find-the-Runner-Up-Score.py
+++ Python/Basic Data Types/Find-the-Runner-Up-Score.py
@@ -1,26 +1,26 @@
 if __name__ == '__main__':
-    n = int(raw_input())
-    
+    n = int(input())
+
     if 2 <= n and n <= 10:
-        
+
         def inputCheck(num):
             num = int(num)
             if -100 <= num and num <= 100:
                 return num
-            
-        arr = map(inputCheck, raw_input().split())[0:n]
+
+        arr = map(inputCheck, input().split())[0:n]
         elements = list(arr)
-        
+
         if len(elements) == n:
-            
+
             maxElement = max(elements)
             lessThanMax = None
-            
+
             for element in elements:
                 if maxElement > element:
                     if (lessThanMax is None):
                         lessThanMax = element
                     elif(lessThanMax < element):
                         lessThanMax = element
-            
+
             print(lessThanMax)
