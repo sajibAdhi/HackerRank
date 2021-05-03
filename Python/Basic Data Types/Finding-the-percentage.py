f533142a6c71bbb29292cd98fe232f80e58f431d
sajibAdhi <60180521+Shiftgithub@users.noreply.github.com>
2021-05-03 21:16:18 +0600

    Finding the percentage

--- Python/Basic Data Types/Finding-the-percentage.py
+++ Python/Basic Data Types/Finding-the-percentage.py
@@ -1,20 +1,21 @@
 if __name__ == '__main__':
     n = int(input())
-    if (n < 2 or n > 10): exit()
-    
+    if (n < 2 or n > 10):
+        exit()
+
     student_marks = {}
     for _ in range(n):
         name, *line = input().split()
         scores = list(map(float, line))
         student_marks[name] = scores
     query_name = input()
-    
-    total_mark = 0;
-    num_of_subject = 0;
-    
+
+    total_mark = 0
+    num_of_subject = 0
+
     for mark in student_marks[query_name]:
         total_mark = total_mark + mark
         num_of_subject = num_of_subject + 1
-    
-        float_formate =  "{:.2f}"
+
+        float_formate = "{:.2f}"
     print(float_formate.format(total_mark / num_of_subject))
